720fc25f682a30a34fb1eebcbc74e7cdd47b899f
A. Drake <a.l.drake713@gmail.com>
2015-03-03 13:55:30 -0500

    Used a front end meanjs generator to add some functionality. Got recipes to display on front page as well.

---
+++ meanjs-generator/app/models/recipe.server.model.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Recipe Schema
+ */
+var RecipeSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Recipe name',
+		trim: true
+	},
+	description: {
+		type: String,
+		default: '',
+	},
+	mealType: {
+		type: String,
+		default: '',
+	},
+	totalTime: {
+		type: String,
+		default: '',
+	},
+	ingredients: {
+		type: Array,
+		default: '',
+		required: 'Please list the ingredients.'
+	},
+	steps: {
+		type: Array,
+		default: '',
+		required: 'Please list how to make this recipe.'
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	isPrivate: {
+		type: Boolean,
+		default: false,
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Recipe', RecipeSchema);
--- meanjs-generator/public/modules/users/controllers/authentication.client.controller.js
+++ meanjs-generator/public/modules/users/controllers/authentication.client.controller.js
@@ -23,9 +23,9 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 			$http.post('/auth/signin', $scope.credentials).success(function(response) {
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
+   				//And redirect to the recipes page
+   				$location.path('/recipes');
 
-				// And redirect to the index page
-				$location.path('/');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
